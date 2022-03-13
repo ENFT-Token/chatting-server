c0ec5b8d827c3dcc855a4541a12ce7e91edc9079
Long9725 <develop9725@gmail.com>
2022-03-14 01:10:57 +0900

    Add: image event

--- app.js
+++ app.js
@@ -3,25 +3,36 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
 
+require('moment-timezone');
+var moment = require('moment');
+moment.tz.setDefault("Asia/Seoul");
+
 app.get('/', function(req, res) {
     res.send('hello world');
 });
+
 io.on('connection',(socket) => {
-    console.log("conn")
-    socket.on('send',(data) =>{
-        console.log("send:",data);
+    console.log('connetion')
+    // send data 
+    socket.on('message',(data) =>{
+        console.log('message:',data)
 
-        socket.emit("send",data);
+        // send data from sender except sender
+        socket.broadcast.emit('message',data);
+        // test
+        // socket.emit("receive", {"id": socket.id, "message":"응답", "username": "강선규", "sendAt": moment().format('YYYY-MM-DD HH:mm:ss')})
     })
 
-    socket.on(('reconnect'), ()=> {
-        console.log("reconnect")
-    })
+    socket.on('image', (data)=>{
+        console.log('image:', data)
 
+        socket.broadcast.emit('image', data);
+    })
     socket.on('disconnect', (reason) => {
-        console.log("disconnect:", reason);
+        console.log("disconnect:", socket.id);
     })
 })
+
 server.listen(3000, function() {
     console.log('Socket IO server listening on port 3000');
 });
